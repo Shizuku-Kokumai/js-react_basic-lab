c7c89eb47e45e6091a2760126843d193006f7355
Shizuku Kokumai <bp20005@shibaura-it.ac.jp>
2022-10-13 20:38:16 +0900

    Exercise 2 completed.

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -3,8 +3,43 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
 const Demo = () => {
+  const modules = [
+    {
+      name: "DevOps",
+      nolec: 2,
+      noprac: 2
+    },
+    {
+      name: "Enterprise Web Dev",
+      nolec: 3,
+      noprac: 2
+    }
+  ];
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>HDip Computer Science - Modules table</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>no lectures</th>
+            <th>no practicals</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].nolec}</td>
+            <td>{modules[0].noprac}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].nolec}</td>
+            <td>{modules[1].noprac}</td>
+          </tr>
+        </tbody >
+      </table>
+    </div>
   );
 };
 
