17ded99ea75f7b252f796d4f3773db18c1b3c18d
Shizuku Kokumai <bp20005@shibaura-it.ac.jp>
2022-10-14 00:59:29 +0900

    Exercise 4 completed.

--- src/components/exercises/04_iteration.js
+++ src/components/exercises/04_iteration.js
@@ -2,10 +2,34 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = props => {
+  const list = props.modules.map((module, index) => {
+    return (
+      <tr key={index} >
+        <th>{module.name}</th>
+        <th>{module.noLectures}</th>
+        <th>{module.noPracticals}</th>
+      </tr>
+    );
+  })
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>{`${props.course}`}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>no lectures</th>
+            <th>no practicals</th>
+          </tr>
+        </thead>
+        <tbody >
+          {list}
+        </tbody >
+      </table>
+    </div>
   );
+
 };
 
 export default Demo;
