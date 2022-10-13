d2c2e9c79a8fb57108f566400d0bb8b1261f0827
Shizuku Kokumai <bp20005@shibaura-it.ac.jp>
2022-10-13 20:23:44 +0900

    Exercise 1 completed.

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,27 +3,30 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-    <table className="table table-borderd">
-      <thread>
-        <tr>
-          <th>Name</th>
-          <th>no lectures</th>
-          <th>no practicals</th>
-        </tr>
-      </thread>
-      <tbody>
-        <tr>
-          <td>devops</td>
-          <td>2</td>
-          <td>2</td>
-        </tr>
-        <tr>
-          <td>enterprise web dev</td>
-          <td>3</td>
-          <td>2</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <h2>HDip Computer Science - Modules table</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>no lectures</th>
+            <th>no practicals</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr>
+            <td>devops</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>enterprise web dev</td>
+            <td>3</td>
+            <td>2</td>
+          </tr>
+        </tbody >
+      </table>
+    </div>
   );
 };
 
